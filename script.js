989e64a8e1143f7755127e3e0632200ff8d93568
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-01-17 18:37:51 -0500

    added color picker and slider

--- script.js
+++ script.js
@@ -2,40 +2,34 @@ const CANVAS_WIDTH = 960;
 const DEFAULT_BLOCKS = 16;
 
 let container = document.getElementById('canvas-container');
+let btnRGB = document.getElementById('btn-RGB');
+let btnBlack = document.getElementById('btn-Black');
+let btnErase = document.getElementById('btn-Erase');
+let btnClear = document.getElementById('btn-Clear');
+let cellSlider = document.getElementById('cellslider');
 
 function createGrid(numRows) {
     let blockSize = Math.floor(CANVAS_WIDTH/numRows);
-    
+    container.style.gridTemplateColumns = `repeat(${numRows}, 1fr)`;   
+    container.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
 
     for (var i = 0; i < numRows**2; i++) {
         let blockDiv = document.createElement('div');
 
         blockDiv.classList.add('canvas');
         blockDiv.style.backgroundColor = 'white';
-        // blockDiv.style.border = '1px solid black';
-        // blockDiv.style.width = blockSize;
-        // blockDiv.style.height = blockSize;
-
-        // canvasContainer_div.style.setProperty('--grid-rows', numOfBlocks);
-        // canvasContainer_div.style.setProperty('--grid-cols', numOfBlocks);
-        
-        drawBlack();
         container.appendChild(blockDiv);
-
-        container.style.gridTemplateColumns = `repeat(${numRows}, minmax(0, 1fr))`;   
-        container.style.gridTemplateRows = `repeat(${numRows}, minmax(0, 1fr))`;
-        
     }
+
+    drawBlack();
 }
 
-function clear(){
-    for (var i = 0; i < numOfBlocks**2; i++) {
-        blockDiv.style.backgroundColor = 'white';
+function clearGrid(){
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
+    createGrid(16); //change to update based on slider
 }
-
-// EVENT LISTENERS
-createGrid(16);
 
 function drawBlack() {
     let blockDivs = document.querySelectorAll('.canvas');
@@ -45,3 +39,43 @@ function drawBlack() {
         })
     })
 }
+
+function erase() {
+    let blockDivs = document.querySelectorAll('.canvas');
+    blockDivs.forEach(blk => {
+        blk.addEventListener('mouseover', () => {
+            blk.style.backgroundColor = 'white';
+        })
+    })
+}
+
+function drawRGB() {
+    let blockDivs = document.querySelectorAll('.canvas');
+    blockDivs.forEach(blk => {
+        blk.addEventListener('mouseover', () =>{
+            randR = Math.floor(Math.random() * 256);
+            randG = Math.floor(Math.random() * 256);
+            randB = Math.floor(Math.random() * 256);
+            blk.style.backgroundColor = `rgb(${randR}, ${randG}, ${randB})`;
+        })
+    })
+}
+
+createGrid(16);
+
+// Event Listeners
+btnRGB.addEventListener('click', () => {
+    drawRGB();
+})
+
+btnBlack.addEventListener('click', () => {
+    drawBlack();
+})
+
+btnClear.addEventListener('click', () => {
+    clearGrid();
+})
+
+btnErase.addEventListener('click', () => {
+    erase();
+})
