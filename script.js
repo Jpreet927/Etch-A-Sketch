2934f52712ac6007b1c196f9bfef67d2f17b03a8
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-01-17 19:43:39 -0500

    fixed issue where grid was being generated twice after slide change

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const CANVAS_WIDTH = 960;
 const DEFAULT_BLOCKS = 16;
+var numRows = 16;
 
 let container = document.getElementById('canvas-container');
 let btnRGB = document.getElementById('btn-RGB');
@@ -8,19 +9,20 @@ let btnErase = document.getElementById('btn-Erase');
 let btnClear = document.getElementById('btn-Clear');
 let cellSlider = document.getElementById('cellslider');
 
-function createGrid(numRows) {
-    let blockSize = Math.floor(CANVAS_WIDTH/numRows);
-    container.style.gridTemplateColumns = `repeat(${numRows}, 1fr)`;   
-    container.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
+function createGrid(gridSize) {
+    
+    // let blockSize = Math.floor(CANVAS_WIDTH/gridSize);
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;   
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-    for (var i = 0; i < numRows**2; i++) {
+    for (var i = 0; i < gridSize**2; i++) {
         let blockDiv = document.createElement('div');
 
         blockDiv.classList.add('canvas');
         blockDiv.style.backgroundColor = 'white';
         container.appendChild(blockDiv);
     }
-
+    
     drawBlack();
 }
 
@@ -28,7 +30,8 @@ function clearGrid(){
     while (container.firstChild) {
         container.removeChild(container.lastChild);
     }
-    createGrid(16); //change to update based on slider
+    console.log(numRows);
+    createGrid(numRows); //change to update based on slider
 }
 
 function drawBlack() {
@@ -61,7 +64,37 @@ function drawRGB() {
     })
 }
 
-createGrid(16);
+function drawColour(colour) {
+    let blockDivs = document.querySelectorAll('.canvas');
+    switch (colour) {
+        case 'red':
+            colour = 'rgba(228, 35, 61, 0.836)';
+            break;
+        case 'orange':
+            colour = 'rgb(255, 157, 65)';
+            break;
+        case 'yellow':
+            colour = 'rgb(255, 222, 75)';
+            break;
+        case 'green':
+            colour = 'rgba(73, 202, 94, 0.925)';
+            break;
+        case 'blue':
+            colour = 'rgb(51, 127, 226)';
+            break;
+        case 'purple':
+            colour = 'rgb(100, 45, 230)';
+            break;
+    }
+
+    blockDivs.forEach(blk => {
+        blk.addEventListener('mouseover', () => {
+            blk.style.backgroundColor = colour;
+        })
+    })
+}
+
+createGrid(25);
 
 // Event Listeners
 btnRGB.addEventListener('click', () => {
@@ -78,4 +111,40 @@ btnClear.addEventListener('click', () => {
 
 btnErase.addEventListener('click', () => {
     erase();
+})
+
+cellSlider.addEventListener('mouseup', () => {
+    numRows = cellSlider.value;
+    clearGrid();
+    console.log(numRows);
+})
+
+btnRed = document.querySelector('.red');
+btnRed.addEventListener('click', () => {
+    drawColour('red');
+})
+
+btnOrange = document.querySelector('.orange');
+btnOrange.addEventListener('click', () => {
+    drawColour('orange');
+})
+
+btnYellow = document.querySelector('.yellow');
+btnYellow.addEventListener('click', () => {
+    drawColour('yellow');
+})
+
+btnGreen = document.querySelector('.green');
+btnGreen.addEventListener('click', () => {
+    drawColour('green');
+})
+
+btnBlue = document.querySelector('.blue');
+btnBlue.addEventListener('click', () => {
+    drawColour('blue');
+})
+
+btnPurple = document.querySelector('.purple');
+btnPurple.addEventListener('click', () => {
+    drawColour('purple');
 })
